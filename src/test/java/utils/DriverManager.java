6befafa30493954831898b7a8698b2ac3154f1c6
klsevf <klsevf@inf.elte.hu>
2025-06-01 23:15:07 +0400

    Modify DriverManager to customize ChromeOptions for WebDriver configuration

--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -2,6 +2,7 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverManager {
@@ -9,8 +10,21 @@ public class DriverManager {
 
     public WebDriver setup() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Modify browser options here:
+        options.addArguments("--disable-notifications");        // Disable browser notifications
+        options.addArguments("--disable-infobars");              // Disable info bars
+        options.addArguments("--start-maximized");               // Start maximized (optional, already done by manage().window())
+        options.addArguments("--incognito");                      // Open in incognito mode
+        // options.addArguments("--headless");                    // Uncomment to run Chrome in headless mode
+
+        driver = new ChromeDriver(options);
+
+        // Optionally, you can also set window size manually:
+        // driver.manage().window().setSize(new Dimension(1920, 1080));
+
         return driver;
     }
 
